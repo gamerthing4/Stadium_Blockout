aca0dfd3018f9cd27522ec0444f742c9fd1e4c3b
vamsi230 <veerendra.dharmavarapu@gmail.com>
2026-09-10 16:19:42 +0530

    Updating

--- Source/Stadium_Blockout/GAS/VS_AttributeSet.h
+++ Source/Stadium_Blockout/GAS/VS_AttributeSet.h
@@ -7,10 +7,11 @@
 #include "AbilitySystemComponent.h"
 #include "VS_AttributeSet.generated.h"
 
-/**
- * 
- * 
- */
+
+DECLARE_DYNAMIC_DELEGATE_TwoParams(FOnHealthChanged, float, "Current Value", float, "Old Value" );
+
+
+
 UCLASS()
 class STADIUM_BLOCKOUT_API UVS_AttributeSet : public UAttributeSet
 {
@@ -18,19 +19,19 @@ class STADIUM_BLOCKOUT_API UVS_AttributeSet : public UAttributeSet
 	
 public:
 	
-	UPROPERTY(EditDefaultsOnly, Category="Attributes")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Attributes")
 	FGameplayAttributeData CurrentHealth = 100.f;
 	ATTRIBUTE_ACCESSORS_BASIC(UVS_AttributeSet, CurrentHealth)
 	
-	UPROPERTY(EditDefaultsOnly, Category="Attributes")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Attributes")
 	FGameplayAttributeData MaxHealth = 100.f;
 	ATTRIBUTE_ACCESSORS_BASIC(UVS_AttributeSet, MaxHealth)
 	
-	UPROPERTY(EditDefaultsOnly, Category="Attributes")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Attributes")
 	FGameplayAttributeData CurrentStamina = 100.f;
 	ATTRIBUTE_ACCESSORS_BASIC(UVS_AttributeSet, CurrentStamina)
 	
-	UPROPERTY(EditDefaultsOnly, Category="Attributes")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Attributes")
 	FGameplayAttributeData MaxStamina = 100.f;
 	ATTRIBUTE_ACCESSORS_BASIC(UVS_AttributeSet, MaxStamina)
 	
